42f62dc8bfbea51a012b8e9c09e739f5bd8d32f0
UnnamedXAer <unnamedxaer.git@gmail.com>
2022-10-11 22:45:02 +0200

    add flavors
    build with flavors;
    fix app name & application id;

---
+++ lib/helpers.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:qrcode_keeper/services/database.dart';
+
+Future<void> initializeApp() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  return Future.wait([
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]),
+    initializeDateFormatting(),
+    DBService.initialize(),
+  ]);
+}
