182a9923966edd75e1ae17175930dd8932cf6c42
UnnamedXAer <UnnamedXAer@gmail.com>
2022-10-13 15:23:29 +0200

    update notification scheduling

--- lib/services/local_notifications_service.dart
+++ lib/services/local_notifications_service.dart
@@ -5,6 +5,7 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_native_timezone/flutter_native_timezone.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
+import 'package:qrcode_keeper/extensions/date_time.dart';
 
 @pragma('vm:entry-point')
 void notificationTapBackground(NotificationResponse notificationResponse) {
@@ -21,7 +22,6 @@ void notificationTapBackground(NotificationResponse notificationResponse) {
 }
 
 class LocalNotificationsService {
-  static const kDailyQrReminderId = 0;
   static final LocalNotificationsService _instance =
       LocalNotificationsService._internal();
   late final FlutterLocalNotificationsPlugin _localNotificationsPlugin;
@@ -41,6 +41,9 @@ class LocalNotificationsService {
       linux: null,
     );
 
+    // not awaited for now, if any problems occurs we will have to await that.
+    _instance._configureLocalTimeZone();
+
     return _instance._localNotificationsPlugin.initialize(
       initializationSettings,
       onDidReceiveNotificationResponse: (notificationResponse) async {
@@ -58,150 +61,68 @@ class LocalNotificationsService {
     );
   }
 
-  Future<void> show({
-    required int id,
-    String? title,
-    String? body,
-    String? payload,
-  }) {
-    assert(
-      id != kDailyQrReminderId,
-      'id of value: $kDailyQrReminderId is reserved for daily qr reminder',
-    );
-
-    const NotificationDetails notificationDetails = NotificationDetails(
-      android: AndroidNotificationDetails(
-        'kt.qrcode_keeper.instant_notification',
-        'QR Keeper Instant Notification',
-        channelDescription: 'This is instant notification with any message.',
-        importance: Importance.max,
-        priority: Priority.high,
-        ticker: 'QR Keeper - instant notification.',
-      ),
-    );
-
-    return _localNotificationsPlugin.show(
-      id,
-      title,
-      body,
-      notificationDetails,
-      payload: payload,
-    );
+  Future<void> _configureLocalTimeZone() async {
+    tz.initializeTimeZones();
+    final String timeZone = await FlutterNativeTimezone.getLocalTimezone();
+    tz.setLocalLocation(tz.getLocation(timeZone));
   }
 
-  Future<void> showTZScheduled({
-    required int id,
-    required Time notificationTime,
+  Future<void> showTZScheduledForDays({
+    required Set<int> days,
+    required TimeOfDay notificationTime,
     String? title,
     String? body,
     String? payload,
   }) async {
-    const NotificationDetails notificationDetails = NotificationDetails(
-      android: AndroidNotificationDetails(
-        'kt.qrcode_keeper.daily_remainder_use_qr_code',
-        'QR Keeper Daily Remainder: Go use QR code',
-        channelDescription: 'This is a daily remainder about using a QR code.',
-        importance: Importance.max,
-        priority: Priority.max,
-        ticker: 'QR Keeper - daily remainder: use QR code.',
-        autoCancel: true,
-        audioAttributesUsage: AudioAttributesUsage.notification,
-        color: Colors.lightGreen,
-        colorized: true,
-        category: AndroidNotificationCategory.reminder,
-        ledColor: Colors.green,
-        ledOnMs: 250,
-        ledOffMs: 1000,
-        enableVibration: true,
-        visibility: NotificationVisibility.private,
-        playSound: true,
-        actions: <AndroidNotificationAction>[
-          AndroidNotificationAction(
-            'dismiss',
-            'Dismiss',
-            cancelNotification: true,
-            showsUserInterface: false,
-            contextual: false,
-          ),
-        ],
-      ),
-    );
-// TODO: here
-    tz.initializeTimeZones();
-    final String? timeZoneName = await FlutterNativeTimezone.getLocalTimezone();
-    final location = tz.getLocation(timeZoneName!);
-    tz.setLocalLocation(location);
-    final scheduledDate = tz.TZDateTime.now(location).add(
-      Duration(seconds: 10),
-    );
+    await _localNotificationsPlugin.cancelAll();
 
-    return _localNotificationsPlugin.zonedSchedule(
-      id,
-      title,
-      body,
-      scheduledDate,
-      notificationDetails,
-      payload: payload,
-      androidAllowWhileIdle: true,
-      uiLocalNotificationDateInterpretation:
-          UILocalNotificationDateInterpretation.wallClockTime,
-      matchDateTimeComponents: DateTimeComponents.dayOfWeekAndTime,
-    );
+    // TODO: pass days by parameter
+    final Set<int> _days = {
+      DateTime.monday,
+      DateTime.tuesday,
+      DateTime.wednesday,
+      DateTime.thursday,
+      DateTime.friday,
+    };
+
+    if (_days.isEmpty) {
+      return;
+    }
+
+    for (var day in _days) {
+      final notificationDetails = _getWeekDayNotificationDetails();
+      final scheduledDate = _getInstanceOfDateTimeFromTimeOfDay(
+        day,
+        notificationTime,
+      );
+
+      final payload =
+          'Notif. scheduled at ${tz.TZDateTime.now(tz.local).format(withSeconds: true)} for 🔔 ${scheduledDate.format()} / $day + ${notificationTime.hour}:${notificationTime.minute}';
+
+      log(payload);
+
+      await _localNotificationsPlugin.zonedSchedule(
+        // day as Id ensures we have at most 1 scheduled notification per week day
+        day,
+        title,
+        body,
+        payload: payload,
+        scheduledDate,
+        notificationDetails,
+        androidAllowWhileIdle: true,
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime,
+        matchDateTimeComponents: DateTimeComponents.dayOfWeekAndTime,
+      );
+    }
   }
-  // Future<void> showScheduled({
-  //   required int id,
-  //   required Time notificationTime,
-  //   String? title,
-  //   String? body,
-  //   String? payload,
-  // }) {
-  //   const NotificationDetails notificationDetails = NotificationDetails(
-  //     android: AndroidNotificationDetails(
-  //       'kt.qrcode_keeper.daily_remainder_use_qr_code',
-  //       'QR Keeper Daily Remainder: Go use QR code',
-  //       channelDescription: 'This is a daily remainder about using a QR code.',
-  //       importance: Importance.max,
-  //       priority: Priority.max,
-  //       ticker: 'QR Keeper - daily remainder: use QR code.',
-  //       autoCancel: true,
-  //       audioAttributesUsage: AudioAttributesUsage.notification,
-  //       color: Colors.lightGreen,
-  //       colorized: true,
-  //       category: AndroidNotificationCategory.reminder,
-  //       ledColor: Colors.green,
-  //       ledOnMs: 250,
-  //       ledOffMs: 1000,
-  //       enableVibration: true,
-  //       visibility: NotificationVisibility.private,
-  //       playSound: true,
-  //       actions: <AndroidNotificationAction>[
-  //         AndroidNotificationAction(
-  //           'dismiss',
-  //           'Dismiss',
-  //           cancelNotification: true,
-  //           showsUserInterface: false,
-  //           contextual: false,
-  //         ),
-  //       ],
-  //     ),
-  //   );
 
-  //   return _localNotificationsPlugin.showDailyAtTime(
-  //     id,
-  //     title,
-  //     body,
-  //     notificationTime,
-  //     notificationDetails,
-  //     payload: payload,
-  //   );
-  // }
-
-  Future<String> getPendingNotificationRequestText() async {
+  Future<List<String>> getPendingNotificationRequestText() async {
     final notifs =
         await _localNotificationsPlugin.pendingNotificationRequests();
-    String s = '${notifs.length}\n';
+    List<String> s = [];
     for (var n in notifs) {
-      s += ('\n${n.id} / ${n.payload}');
+      s.add('${n.id} / ${n.payload}');
     }
 
     return s;
@@ -221,4 +142,58 @@ class LocalNotificationsService {
     final perm = await imp.requestPermission();
     return perm;
   }
+
+  tz.TZDateTime _getInstanceOfDateTimeFromTimeOfDay(
+    int weekDay,
+    TimeOfDay time,
+  ) {
+    assert(weekDay >= DateTime.monday && weekDay <= DateTime.sunday);
+
+    // 2022-08 -> start at monday (the "day" is 1) so its nicely work with weekDay(s) where monday is represented as 1.
+    // otherwise passing "weekDay" as day would be an error causing incorrect date.
+    tz.TZDateTime scheduledDate = tz.TZDateTime(
+      tz.local,
+      2022,
+      8,
+      weekDay,
+      time.hour,
+      time.minute,
+    );
+
+    return scheduledDate;
+  }
+
+  NotificationDetails _getWeekDayNotificationDetails() =>
+      const NotificationDetails(
+        android: AndroidNotificationDetails(
+          'kt.qrcode_keeper.daily_remainder_use_qr_code',
+          'QR Keeper Daily Remainder: Go use QR code',
+          channelDescription:
+              'This is a daily remainder about using a QR code.',
+          importance: Importance.max,
+          priority: Priority.max,
+          ticker: 'QR Keeper - daily remainder: use QR code.',
+          autoCancel: true,
+          audioAttributesUsage: AudioAttributesUsage.notification,
+          color: Colors.lightGreen,
+          colorized: true,
+          category: AndroidNotificationCategory.reminder,
+          ledColor: Colors.green,
+          ledOnMs: 250,
+          ledOffMs: 1000,
+          enableVibration: true,
+          visibility: NotificationVisibility.private,
+          playSound: true,
+          onlyAlertOnce: true,
+          actions: <AndroidNotificationAction>[
+            AndroidNotificationAction(
+              'dismiss',
+              'Dismiss',
+              cancelNotification: true,
+              showsUserInterface: false,
+              contextual: false,
+            ),
+          ],
+        ),
+      );
 }
